685700fe25bb53a0e13b00cda0e3cfa08772c1c5
konguks <konguskarthick@gmail.com>
2015-08-03 01:31:08 +0530

    latest

--- NewWorld.php
+++ NewWorld.php
@@ -20,11 +20,17 @@ class NewWorld
         return $a-$b;
     }
     public function GetAPICon(){
-        define('CONSUMER_KEY', 'tEOWFrYWgs4ml0PzJtKSsYUrj');
-        define('CONSUMER_SECRET', 'o58Tp63xUnNkIJUIr9o96O47kwhvNq1amRciv5F5MtdTLPwX1z');
-        define('ACCESS_TOKEN', '714419437-qTbbCQGAq3fx3Hn6RDpv7MZ6CA78BLwDxWlMAY7O');
-        define('ACCESS_TOKEN_SECRET', 'bX6sQfSD9MNKez83VAjKWOq7bxWc8DVxsOmTgTOjXuxwi');
-        return new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+
+        #define('CONSUMER_KEY', 'tEOWFrYWgs4ml0PzJtKSsYUrj');
+        $CONSUMER_KEY = 'tEOWFrYWgs4ml0PzJtKSsYUrj';
+        #define('CONSUMER_SECRET', 'o58Tp63xUnNkIJUIr9o96O47kwhvNq1amRciv5F5MtdTLPwX1z');
+        $CONSUMER_SECRET = 'o58Tp63xUnNkIJUIr9o96O47kwhvNq1amRciv5F5MtdTLPwX1z';
+        #define('ACCESS_TOKEN', '714419437-qTbbCQGAq3fx3Hn6RDpv7MZ6CA78BLwDxWlMAY7O');
+        $ACCESS_TOKEN = '714419437-qTbbCQGAq3fx3Hn6RDpv7MZ6CA78BLwDxWlMAY7O';
+        #define('ACCESS_TOKEN_SECRET', 'bX6sQfSD9MNKez83VAjKWOq7bxWc8DVxsOmTgTOjXuxwi');
+        $ACCESS_TOKEN_SECRET = 'bX6sQfSD9MNKez83VAjKWOq7bxWc8DVxsOmTgTOjXuxwi';
+
+        return new TwitterOAuth($CONSUMER_KEY, $CONSUMER_SECRET, $ACCESS_TOKEN, $ACCESS_TOKEN_SECRET);
     }
     public function GetSearchTweet($toa,$query){
 
